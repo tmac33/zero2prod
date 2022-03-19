80b6f6e31f72bf23d6777d70da40d0c936cbbcd3
tmac33 <therealtmac33@gmail.com>
2022-03-20 12:13:09 +1300

    test intergration

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app.");
+    spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
@@ -15,6 +15,7 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background ~somehow~
-async fn spawn_app() -> std::io::Result<()> {
-    zero2prod::run().await
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to spawn our app.");
+    let _ = tokio::spawn(server);
 }
